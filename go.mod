f513e7eb6e849ff6b5c3862a985524d3a9188c01
Raj Kondapuli (Contractor) <Raj.Kondapuli@wnco.com>
2022-11-23 06:29:03 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/srirajkondapuli/mylogger
+
+go 1.19
---
+++ logg.go
@@ -0,0 +1,11 @@
+package mylogger
+
+import (
+	"fmt"
+)
+
+var Version string = "1.0"
+
+func Log(mess string) {
+	fmt.Println("[LOG] " + mess)
+}
